d2d3cbd797e6c88274d260a8adf151c6559e0730
KucherenkoRoman <roman.kucherenko1999@gmail.com>
2021-02-26 01:25:56 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 module.exports = function toReadable (number) {
+  const nums = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen"
+  ];
+
+  const tensNums = [
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety"
+  ];
   
+  if (number === 0){
+      return "zero";
+  }
+
+  if (number < 20){
+      return nums[number - 1];
+  }
+
+  if (number < 100){
+    return tensNums[Math.floor(number / 10) - 1] + (number % 10 === 0 ? "" : " " + nums[(number % 10) - 1]);
+  }
+
+  if (number < 1000){
+      let readableNumber = "";
+      readableNumber += nums[Math.floor(number / 100) - 1] + " hundred";
+
+      if (number % 100 === 0){
+          return readableNumber;
+      }
+
+      if (number % 100 < 20) {
+        return readableNumber + " " + nums[(number % 100) - 1]
+      }
+
+      return readableNumber + " " + tensNums[Math.floor((number % 100) / 10) - 1] + ((number % 100) % 10 === 0 ? "" : " " + nums[((number % 100) % 10) - 1])
+  }
 }
